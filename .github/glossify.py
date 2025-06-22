09010e56d93ed4f1f018e12d6184a0fd6e3826fb
IfJay <andreajsm03@gmail.com>
2025-06-22 14:57:15 +0200

    abbr

--- .github/glossify.py
+++ .github/glossify.py
@@ -16,33 +16,32 @@ def main():
         os.chdir(path.Path(init_path+docs))
         for doc in os.listdir(path.Path(".")):
             Glossify(path.Path(init_path+docs+"/"+doc))
-            # ApplyAbbr(path.Path(init_path+docs+"/"+doc))
 
 def GetYMLKeys(yml_path):
     with open(yml_path,"r") as g:
         yml = yaml.safe_load(g)
-    return yml.keys()
+    return list(yml.keys())
 
 def Glossify(doc_path):
     glossary = GetYMLKeys(doc_path / "yml/glossary.yml")
+    abbrs = GetYMLKeys(doc_path / "yml/abbreviation.yml")
+
+    words = set(glossary+abbrs)
+
     sections_path= doc_path/"sections"
+
     for section in os.listdir(sections_path):
         if section in IGNORE_SECTION:
             continue
+        
         s_path = sections_path/section
         original_text=s_path.read_text(encoding="utf-8")
         
         modified_text = original_text
-        for word in glossary:
+        for word in words:
             modified_text=modified_text.replace(word,"#glos(\""+word+"\")") 
         
         s_path.write_text(modified_text,encoding="utf-8")
-
-
-
-def ApplyAbbr(doc_path):
-    abbrs = GetYMLKeys(doc_path / "yml/abbreviation.yml")
-    print(abbrs)
 
 if __name__ == "__main__":
     main()
